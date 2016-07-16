2301b23a3ab5bf95e1124060fb6725e60e25acaa
hellomundo <ed.ortega@gmail.com>
2016-07-15 17:46:29 -0700

    basic CRUD for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,25 @@
 class TasksController < ApplicationController
 
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
-
+    @tasks = Task.all.order("created_at DESC")
   end
 
   def show
-
   end
 
   def new
-
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
 
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -21,21 +27,26 @@ class TasksController < ApplicationController
   end
 
   def update
-
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def find_task
-
+    @task = Task.find(params[:id])
   end
 
   def task_params
-
+    params.require(:task).permit(:hours, :notes)
   end
 
 end
