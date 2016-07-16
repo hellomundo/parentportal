aedd987f8c4e236602234320e28c397302338efe
hellomundo <ed.ortega@gmail.com>
2016-07-15 20:47:54 -0700

    added association between users and tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716033553) do
+ActiveRecord::Schema.define(version: 20160716034205) do
 
   create_table "tasks", force: :cascade do |t|
     t.decimal  "hours"
     t.text     "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
