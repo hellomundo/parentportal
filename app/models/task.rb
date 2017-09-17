83a283798097ff8f4e38c152df45c84b5540ea0f
hellomundo <ed.ortega@gmail.com>
2017-09-17 13:40:25 -0700

    require user, family and task type now

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
-  belongs_to :user
-  belongs_to :family
-  belongs_to :task_type
+  belongs_to :user, required: true
+  belongs_to :family, required: true
+  belongs_to :task_type, required: true
 
   validates :hours, presence: true
   validates :hours, numericality: true
