539a3ee5f3c8ad3a51d495e762f1229d6efde358
Edmundo Ortega <ed.ortega@gmail.com>
2023-08-25 17:29:22 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
   helper_method :sort_column, :sort_direction
 
   before_action :authenticate_user!
